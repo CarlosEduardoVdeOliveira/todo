2d69ba644b25cb2c1c7660fdeb8eb2b754c37347
Carlos Eduardo <vieiradeoliveira70@gmail.com>
2022-10-21 10:24:44 -0300

    add function saved in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
@@ -12,8 +12,19 @@ export interface ITask {
 
 function App() {
   const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function setTasksSaveInLocalStorage(setNewTask: ITask[]) {
+    setTasks(setNewTask);
+    localStorage.setItem("todo:list", JSON.stringify(setNewTask));
+  }
+  function getTasksInLocalStorage() {
+    const todoList = localStorage.getItem("todo:list");
+    if (todoList) {
+      setTasks(JSON.parse(todoList));
+    }
+  }
   function addTask(content: string) {
-    setTasks([
+    setTasksSaveInLocalStorage([
       ...tasks,
       {
         id: uuidv4(),
@@ -24,7 +35,7 @@ function App() {
   }
   function deleteTask(id: string) {
     const newTasks = tasks.filter((task) => task.id !== id);
-    setTasks(newTasks);
+    setTasksSaveInLocalStorage(newTasks);
   }
   function checkedIsCompletedTask(id: string) {
     const checked = tasks.map((task) => {
@@ -36,8 +47,11 @@ function App() {
       }
       return task;
     });
-    setTasks(checked);
+    setTasksSaveInLocalStorage(checked);
   }
+  useEffect(() => {
+    getTasksInLocalStorage();
+  }, []);
   return (
     <div>
       <Header onAddTask={addTask} />
